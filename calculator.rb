d38f6959a54586f458f809279fc0e3a4dd6c7a8b
Hashrocket Workstation <dev@hashrocket.com>
2013-12-10 12:38:23 -0500

    Ensure Calculator.add actually does something

--- calculator.rb
+++ calculator.rb
@@ -2,14 +2,19 @@ require 'rspec'
 
 class Calculator
   def self.add(a,b)
-    4
+    a + b
   end
 end
 
 describe Calculator do
   describe "#add" do
-    it "adds two numbers" do
+    it "returns 4 as the result of 2 plus 2" do
       expect(Calculator.add(2,2)).to eq(4)
     end
+
+    it "returns 5 as the result of 2 plus 3" do
+      expect(Calculator.add(2,3)).to eq(5)
+    end
+
   end
 end
