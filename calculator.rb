afa3e27b5bdc24b09aa22f1f9a74ef42ec472a24
Hashrocket Workstation <dev@hashrocket.com>
2013-12-10 12:45:20 -0500

    Add fancy new addition interface

--- calculator.rb
+++ calculator.rb
@@ -1,20 +1,31 @@
 require 'rspec'
 
 class Calculator
-  def self.add(a,b)
-    a + b
+  def initialize(addend)
+    @addend = addend
   end
+
+  def self.add(a)
+    Calculator.new(a)
+  end
+
+  def to(value)
+    addend + value
+  end
+
+  private
+
+  attr_reader :addend
 end
 
 describe Calculator do
   describe "#add" do
     it "returns 4 as the result of 2 plus 2" do
-      expect(Calculator.add(2,2)).to eq(4)
+      expect(Calculator.add(2).to(2)).to eq(4)
     end
 
     it "returns 5 as the result of 2 plus 3" do
-      expect(Calculator.add(2,3)).to eq(5)
+      expect(Calculator.add(2).to(3)).to eq(5)
     end
-
   end
 end
